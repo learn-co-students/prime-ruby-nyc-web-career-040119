fd475f8fa6cc5d93d54178ca57d8a822e37b3a8d
Jeff M <jkdmarks@gmail.com>
2019-03-07 16:42:07 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,17 @@
-# Add  code here!
+def prime?(num)
+  output = true
+  if num <= 1
+    output = false
+  elsif num == 2 || num == 3
+    output = true
+  else
+    for i in (2..num-1).to_a
+      if num % i == 0
+        output = false
+      end
+    end
+  end
+  output
+end
+
+prime?(197)
