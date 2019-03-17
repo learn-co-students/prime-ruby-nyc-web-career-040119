6b05ea535eec3ec9b2d23189a088de4f4c8aadad
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-17 15:22:11 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
 # Add  code here!
+def prime?(number)
+  
+  range_number = (1..number) # could do number/2 to shorten loop, however test wants full range
+  factors = []
+  
+  range_number.each do |check|
+    if number % check == 0 
+      factors << check
+    end
+  end
+  
+  if factors.size == 2 #primes only have 2 factors 1 and the number itself
+    true 
+  else 
+    false 
+  end 
+end 
