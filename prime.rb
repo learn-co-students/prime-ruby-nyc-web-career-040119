15aa582ae633849d2d9f1e5b580243c0785c5d0e
Matthew Bartl <mattbartl@gmail.com>
2019-03-19 20:35:04 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+def prime?(number)
+  if number < 2
+    return false
+    
+  elsif number == 2
+    return true
+    
+  else
+    check = (number/2).floor
+    while check > 1 do
+      if number % check == 0
+        return false
+      end
+      check -= 1
+    end
+    return true
+  end
+end
