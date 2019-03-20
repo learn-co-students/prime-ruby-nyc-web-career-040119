9d3c3f0cf3860259d090e5d13366d6ea20f37c95
Troy Gooden <tro12185@gmail.com>
2019-03-20 22:42:12 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,10 @@
-# Add  code here!
+
+def prime?(int)
+  if int < 0 || int == 1 || int == 0 
+    false
+  else
+    (2...int).all? do |findprime|
+      int % findprime != 0
+    end 
+  end 
+end 
