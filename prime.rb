87817615c92b01b4bb576c0dd0e1da470b6b4077
virginia koar <virginiakoar@TimKoar.fios-router.home>
2019-03-20 13:09:24 -0400

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,14 @@
-# Add  code here!
+
+def prime?(num)
+  if num <= 1 # any number that is <=1 is not a natural number and thus is not prime.
+    return false # It is determined to be false immediately.
+  end
+  i = 2 # we begin our counter variable value at the first prime number, 2.
+  while i < num # while our counter value is less than the next number in our array, the loop will run.
+    if num % i == 0 # if that number is disible by any value stored in our counter variable, it is not prime.
+    return  false # and will return false
+  end
+    i += 1
+  end
+  true 
+end
